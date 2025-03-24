c9542b4efdd01b5b43eb454ffbc03c864dd411d2
eodyd9684 <eodyd9684@naver.com>
2025-03-24 16:07:27 +0900

    spring boot

---
+++ src/main/java/com/green/farmseye/FarmseyeApplication.java
@@ -0,0 +1,13 @@
+package com.green.farmseye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmseyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmseyeApplication.class, args);
+	}
+
+}
