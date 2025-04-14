a35c5074462758fd7f28ef883630d3a13cf5b826
sungter <eroomeuzz@naver.com>
2025-04-14 17:38:57 +0900

    20250414 18시까지 학원에서 작업한 것

--- src/main/java/com/green/farmseye/environment/controller/EnvironmentController.java
+++ src/main/java/com/green/farmseye/environment/controller/EnvironmentController.java
@@ -1,13 +1,23 @@
 package com.green.farmseye.environment.controller;
 
+import com.green.farmseye.environment.dto.EnvironmentDTO;
 import com.green.farmseye.environment.service.EnvironmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/farms")
 @RequiredArgsConstructor
 public class EnvironmentController {
   private final EnvironmentService environmentService;
+
+  @GetMapping("")
+  private List<EnvironmentDTO> selectEnv(){
+    return environmentService.selectEnv();
+  };
+
 }
